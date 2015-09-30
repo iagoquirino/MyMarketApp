b7c991a39616c3f7c6c5035e09cad3116e403793
Iago Quirino <iago.quirino@hotmail.com>
2015-09-29 22:38:56 -0300

    Purchase View

--- app/src/main/java/br/com/mymarket/fragments/InformacaoComprasFragment.java
+++ app/src/main/java/br/com/mymarket/fragments/InformacaoComprasFragment.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.TextView;
+
 import br.com.mymarket.R;
 import br.com.mymarket.activities.ListaComprasActivity;
 
@@ -15,7 +17,7 @@ public class InformacaoComprasFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     	View view = inflater.inflate(R.layout.fragment_info_compras, container, false);
     	final ListaComprasActivity activity = ((ListaComprasActivity)this.getActivity());
-    	EditText titulo = (EditText) view.findViewById(R.id.lista_compra_nome);
+        TextView titulo = (TextView) view.findViewById(R.id.lista_compra_nome);
     	titulo.setText(activity.getItemSelecionado().getNome());
         return view;
     }
